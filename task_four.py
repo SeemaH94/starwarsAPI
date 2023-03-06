b1e8efea7dbac423e8e56a4681a661ff819a6b59
seema <seemagh92@gmail.com>
2023-03-06 22:02:20 +0530

    update

--- task_four.py
+++ task_four.py
@@ -11,13 +11,17 @@ For example - "A new hope" movie has following resource endpoints -
 - species  40
 """
 
+from multiprocessing.pool import ThreadPool
+from pydantic import parse_obj_as
+from typing import List
+
 from resources.films import Film   # resource model
 from models.datamodels.films import Film_  # pydantic model
 from models.datamodels.charectors import Character_
 
 from dal.db_conn_helper import get_db_conn
 from dal.dml import insert_resource
-from utils.fetch_data import hit_url
+from utils.fetch_data import hit_url, fetch_char_names, fetch_data_v2
 from utils.timing import timeit
 
 
@@ -85,9 +89,16 @@ if __name__ == "__main__":
         film_data.url,
     ]
 
+    # DB operation
     result = insert_resource(
         "film", "film_id", film_data.episode_id, film_columns, film_values
     )
+
+    characters = film_data.characters
+    pool = ThreadPool(5)
+    characters = pool.map(fetch_data_v2, characters)
+    characters = parse_obj_as(List[Character_], characters)
+    breakpoint()
 
     # TODO
     # capture all characters
